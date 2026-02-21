e8f4d9ffc4dd3aad14602ecca7b9b0892f510fa0
ManasvixD <manasvi070btcsai22@igdtuw.ac.in>
2026-02-22 05:08:46 +0530

    Added  solution for  smallest sacred window  in java

---
+++ SlidingWindowandTwoPointers/Smallest Sacred Window/manasvi9745_soln.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        long k = sc.nextLong();
+        long[] arr = new long[n];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = sc.nextLong();
+
+        int left = 0;
+        long sum = 0;
+        int minLength = Integer.MAX_VALUE;
+
+        for (int right = 0; right < n; right++) {
+            sum += arr[right];
+
+            while (sum >= k) {
+                minLength = Math.min(minLength, right - left + 1);
+                sum -= arr[left];
+                left++;
+            }
+        }
+
+        if (minLength == Integer.MAX_VALUE)
+            System.out.println(-1);
+        else
+            System.out.println(minLength);
+    }
+}
